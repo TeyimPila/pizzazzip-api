fa6c11d7bee60d807ed97cfd31ed4b68daefcd15
Pila <pat@medicuja.com>
2020-05-24 07:37:36 +0200

    refactor code and add documentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,83 +1,83 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+    <div class="flex-center position-ref full-heighΩt">
 
-            <div class="content">
-                <div class="title m-b-md">
-                   Welcome to pizzAzzip
-                </div>
+        <div class="content">
+            <div class="title m-b-md">
+                pizzAzzip
+            </div>
 
-                <div class="links">
-                    <a href="#">API Documentation</a>
-                    <a href="https://pizzazzip.herokuapp.com">Frontend</a>
-                    <a href="https://github.com/TeyimPila/pizzazzip-web">Frontend Github Repo</a>
-                    <a href="https://github.com/TeyimPila/pizzazzip-api">Backend Github Repo</a>
-                </div>
+            <div class="links">
+                <a href="#">API Documentation</a>
+                <a href="https://pizzazzip.herokuapp.com">Frontend</a>
+                <a href="https://github.com/TeyimPila/pizzazzip-web">Frontend Github Repo</a>
+                <a href="https://github.com/TeyimPila/pizzazzip-api">Backend Github Repo</a>
             </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
