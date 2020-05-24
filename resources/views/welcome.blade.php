1377b032a1f46099ec6f99e00b6aa58c6a85c9eb
Pila <pat@medicuja.com>
2020-05-24 07:38:29 +0200

    refactor code and add documentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
     </style>
 </head>
 <body>
-    <div class="flex-center position-ref full-heighΩt">
+    <div class="flex-center position-ref full-height">
 
         <div class="content">
             <div class="title m-b-md">
